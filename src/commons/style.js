18345d7b79503561f61e24fb9c640d1921de7acb
LEMBARED NABIL <NLEMBARED@pca.ma>
2019-07-26 11:36:33 +0100

    adding common style for all commponents

---
+++ src/commons/style.js
@@ -0,0 +1,47 @@
+const styles = {
+    radioGroupStyle : {
+        justifyContent: 'space-evenly'
+    },
+    radioColor : {
+        color : "#0012ff",
+        fontSize: 20,
+    },
+    cardStyle : {
+        height: 240,
+        width: 250,
+        borderRadius: 17,
+        boxShadow : '0px 5px 5px -3px #4906e24a, 0px 8px 10px 1px #7e9fed5c, 0px 3px 14px 2px #8fbfe880'
+    },
+    spinnerStyle : {
+        display: 'inline-block',
+        position: 'fixed',
+        margin: 'auto',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: 64,
+        height: 64,
+        backgroundColor: 'white',
+        borderRadius: 3,
+        color : "#0012ff"
+    },
+    textStyle : {
+        fontFamily : "sans-serif",
+        fontSize : 24
+    },
+    buttonBloc : {
+        display: 'flex',
+        borderRadius: '50%',
+        border: '2px solid #0012ff',
+    },
+    backIconBloc : {
+        transform: 'scale(-1.2)',
+        fontSize: '4.1875rem'
+    },
+    nextIconBloc : {
+        fontSize: '4.1875rem'
+    }
+}
+
+export default styles;
